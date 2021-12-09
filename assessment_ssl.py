011c7d42f42910f9aeb997405c0bf89a2747351b
SebastianHafner <hafnersailing@gmail.com>
2021-12-09 11:11:48 +0100

    assessment ssl

--- assessment_ssl.py
+++ assessment_ssl.py
@@ -5,9 +5,10 @@ from tqdm import tqdm
 import argparse
 from pathlib import Path
 from utils import experiment_manager, networks, datasets
+FONTSIZE = 16
 
 
-def qualitative_assessment(cfg: experiment_manager.CfgNode, run_type: str = 'validation'):
+def qualitative_assessment_change(cfg: experiment_manager.CfgNode, run_type: str = 'validation'):
     net, *_ = networks.load_checkpoint(cfg.INFERENCE_CHECKPOINT, cfg, 'cpu')
     net.eval()
     ds = datasets.SpaceNet7CDDataset(cfg, run_type, dataset_mode='first_last', no_augmentations=True,
@@ -15,39 +16,86 @@ def qualitative_assessment(cfg: experiment_manager.CfgNode, run_type: str = 'val
 
     for item in ds:
         aoi_id = item['aoi_id']
-        if aoi_id == 'L15-0571E-1075N_2287_3888_13':
-            x_t1 = item['x_t1']
-            x_t2 = item['x_t2']
-            logits_change, logits_sem_t1, logits_sem_t2 = net(x_t1.unsqueeze(0), x_t2.unsqueeze(0))
-            y_pred_change = torch.sigmoid(logits_change).squeeze().detach()
-            y_pred_sem_t1 = torch.sigmoid(logits_sem_t1).squeeze().detach()
-            y_pred_sem_t2 = torch.sigmoid(logits_sem_t2).squeeze().detach()
+        x_t1 = item['x_t1']
+        x_t2 = item['x_t2']
+        logits_change, logits_sem_t1, logits_sem_t2 = net(x_t1.unsqueeze(0), x_t2.unsqueeze(0))
 
-            gt_change = item['y_change'].squeeze()
-            gt_sem_t1 = item['y_sem_t1'].squeeze()
-            gt_sem_t2 = item['y_sem_t2'].squeeze()
+        fig, axs = plt.subplots(1, 5, figsize=(20, 4))
 
-            fig, axs = plt.subplots(2, 4, figsize=(20, 10))
-            axs[0, 0].imshow(x_t1.numpy().transpose((1, 2, 0)))
-            axs[1, 0].imshow(x_t2.numpy().transpose((1, 2, 0)))
+        axs[0].imshow(x_t1.numpy().transpose((1, 2, 0)))
+        axs[0].set_title(r'Planet $t_1$', fontsize=FONTSIZE)
+        axs[1].imshow(x_t2.numpy().transpose((1, 2, 0)))
+        axs[1].set_title(r'Planet $t_2$', fontsize=FONTSIZE)
 
-            axs[0, 1].imshow(gt_sem_t1.numpy(), cmap='gray')
-            axs[1, 1].imshow(gt_sem_t2.numpy(), cmap='gray')
+        gt_change = item['y_change'].squeeze()
+        axs[2].imshow(gt_change.numpy(), cmap='gray')
+        axs[2].set_title(r'GT', fontsize=FONTSIZE)
 
-            axs[0, 2].imshow(y_pred_sem_t1.numpy(), cmap='gray')
-            axs[1, 2].imshow(y_pred_sem_t2.numpy(), cmap='gray')
+        logits_change_sem = net.outc_sem_change(torch.cat((logits_sem_t1, logits_sem_t2), dim=1))
+        y_pred_change_sem = torch.sigmoid(logits_change_sem).squeeze().detach()
+        axs[3].imshow(y_pred_change_sem.numpy(), cmap='gray')
+        axs[3].set_title(r'Change Sem', fontsize=FONTSIZE)
 
-            axs[0, 3].imshow(gt_change.numpy(), cmap='gray')
-            axs[1, 3].imshow(y_pred_change.numpy(), cmap='gray')
+        y_pred_change = torch.sigmoid(logits_change).squeeze().detach()
+        axs[4].imshow(y_pred_change.numpy(), cmap='gray')
+        axs[4].set_title(r'Change', fontsize=FONTSIZE)
 
-            for _, ax in np.ndenumerate(axs):
-                ax.set_axis_off()
+        for _, ax in np.ndenumerate(axs):
+            ax.set_axis_off()
 
-            out_file = Path(cfg.PATHS.OUTPUT) / 'plots' / f'{aoi_id}.png'
-            plt.savefig(out_file, dpi=300, bbox_inches='tight')
+        out_file = Path(cfg.PATHS.OUTPUT) / 'plots' / 'assessment_change_ssl' / f'{aoi_id}.png'
+        out_file.parent.mkdir(exist_ok=True)
+        plt.savefig(out_file, dpi=300, bbox_inches='tight')
+        plt.close(fig)
 
-            # plt.show()
-            plt.close(fig)
+
+def qualitative_assessment_sem(cfg: experiment_manager.CfgNode, run_type: str = 'validation'):
+    net, *_ = networks.load_checkpoint(cfg.INFERENCE_CHECKPOINT, cfg, 'cpu')
+    net.eval()
+    ds = datasets.SpaceNet7CDDataset(cfg, run_type, dataset_mode='first_last', no_augmentations=True,
+                                     disable_unlabeled=True, disable_multiplier=True)
+
+    for item in ds:
+        aoi_id = item['aoi_id']
+        x_t1 = item['x_t1']
+        x_t2 = item['x_t2']
+        _, logits_sem_t1, logits_sem_t2 = net(x_t1.unsqueeze(0), x_t2.unsqueeze(0))
+
+        fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+
+        axs[0, 0].imshow(x_t1.numpy().transpose((1, 2, 0)))
+        axs[0, 0].set_title(r'Planet $t_1$', fontsize=FONTSIZE)
+
+        gt_sem_t1 = item['y_sem_t1'].squeeze()
+        axs[0, 1].imshow(gt_sem_t1.numpy(), cmap='gray')
+        axs[0, 1].set_title(r'GT $t_1$', fontsize=FONTSIZE)
+
+        y_pred_sem_t1 = torch.sigmoid(logits_sem_t1).squeeze().detach()
+        axs[0, 2].imshow(y_pred_sem_t1.numpy(), cmap='gray')
+        axs[0, 2].set_title(r'Pred $t_1$', fontsize=FONTSIZE)
+
+        axs[1, 0].imshow(x_t2.numpy().transpose((1, 2, 0)))
+        axs[1, 0].set_title(r'Planet $t_2$', fontsize=FONTSIZE)
+
+        gt_sem_t2 = item['y_sem_t2'].squeeze()
+        axs[1, 1].imshow(gt_sem_t2.numpy(), cmap='gray')
+        axs[1, 1].set_title(r'GT $t_2$', fontsize=FONTSIZE)
+
+        y_pred_sem_t2 = torch.sigmoid(logits_sem_t2).squeeze().detach()
+        axs[1, 2].imshow(y_pred_sem_t2.numpy(), cmap='gray')
+        axs[1, 2].set_title(r'Pred $t_1$', fontsize=FONTSIZE)
+
+        for _, ax in np.ndenumerate(axs):
+            ax.set_axis_off()
+
+        out_file = Path(cfg.PATHS.OUTPUT) / 'plots' / 'assessment_sem_ssl' / f'{aoi_id}.png'
+        out_file.parent.mkdir(exist_ok=True)
+        plt.savefig(out_file, dpi=300, bbox_inches='tight')
+        plt.close(fig)
+
+
+def quantitative_assessment(cfg: experiment_manager.CfgNode, run_type: str = 'validation'):
+    pass
 
 
 def assessment_argument_parser():
@@ -72,4 +120,5 @@ def assessment_argument_parser():
 if __name__ == '__main__':
     args = assessment_argument_parser().parse_known_args()[0]
     cfg = experiment_manager.setup_cfg(args)
-    qualitative_assessment(cfg, run_type=args.run_type)
+    qualitative_assessment_change(cfg, run_type=args.run_type)
+    qualitative_assessment_sem(cfg, run_type=args.run_type)
